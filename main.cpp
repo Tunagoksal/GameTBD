473397cb51088c57cdc65c5e00b0b405f857d9a1
Tunagoksal <tuna.goksal@ug.bilkent.edu.tr>
2025-01-15 16:37:03 +0300

    a single pixel?

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <SDL.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc,char* argv[]) {
+
+    SDL_Window* window = nullptr;
+    SDL_Renderer* renderer = nullptr;
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_CreateWindowAndRenderer(640,480,0,&window,&renderer);
+    SDL_RenderSetScale(renderer,1,1);
+
+    SDL_SetRenderDrawColor(renderer,0,0,0,255);
+    SDL_RenderClear(renderer);
+
+    SDL_SetRenderDrawColor(renderer,255,255,255,255);
+    SDL_RenderDrawPoint(renderer,640/2,480/2);
+
+    SDL_RenderPresent(renderer);
+    SDL_Delay(10000);
+
+    SDL_Quit();
     return 0;
 }
